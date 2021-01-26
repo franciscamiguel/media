17f03175b5fbecf9a36afc17db3fd71c1a506d91
franciscamiguel <miguelfrancisca92@gmail.com>
2021-01-26 20:07:39 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int media = 0;
+            int idade1 = 5;
+            int idade2 = 7;
+            int idade3 = 9;
+            int idade4 = 11;
+            int idade5 = 13;
+            media = (idade1 + idade2 + idade3 + idade4 + idade5) / 5;
+            Console.WriteLine(" A idade média dos alunos é " + media);
+            Console.ReadKey();
+        }
+    }        
+}  
